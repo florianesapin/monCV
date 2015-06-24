9330eca990ec893ae9a198bc754a3fb15cd6c562
florianesapin <floriane.sapin@IG14-SapinF.intra.eiaj.ch>
2015-06-24 16:24:10 +0200

    Ajout des liens smoothScroll

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
 console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+		$('a').smoothScroll();
+		$('div.progress').each(function(index, element){
+			//var data = [
+			//{
+			//value: 300,
+			//color:"#F7464A",
+			//highlight: "#FF5A5E",
+			//label: "Red"
+			//}
+			//var myDoughnutChart = new Chart(ctx[1]).Doughnut(data,options);
+			
+			//$(element).css("backgroundColor", "Red");
+		});
+});
