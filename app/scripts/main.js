65b306a33a4cffc12e6114fc374fd202c6a5ede2
florianesapin <floriane.sapin@IG14-SapinF.intra.eiaj.ch>
2015-06-25 14:20:31 +0200

    add DoughnutChart pour les compétences

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,29 @@
 console.log('\'Allo \'Allo!');
 $(document).ready(function(){
 		$('a').smoothScroll();
+			
+		
 		$('div.progress').each(function(index, element){
-			//var data = [
-			//{
-			//value: 300,
-			//color:"#F7464A",
-			//highlight: "#FF5A5E",
-			//label: "Red"
-			//}
-			//var myDoughnutChart = new Chart(ctx[1]).Doughnut(data,options);
+			var taux = $(element).children().attr( "aria-valuenow" );
+			
+			$(element).replaceWith('<canvas id="myChart' + index + '" width="100" height="100"></canvas>');
+			var data = [
+			{
+			value: taux,
+			color:"#F7464A",
+			highlight: "#F7464A",
+			label: "Red",
+			height: "10px"
+			},
+			{
+			value: 100-taux,
+			color: "#FFFFFF",
+			highlight: "#FFFFFF",
+			label: "Withe"
+			}
+			]
+			var ctx = document.getElementById("myChart" + index).getContext("2d");
+			var myDoughnutChart = new Chart(ctx).Doughnut(data,{percentageInnerCutout: 80});
 			
 			//$(element).css("backgroundColor", "Red");
 		});
